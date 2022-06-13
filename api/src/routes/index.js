f9e7aefca4fa611a2eb74eb47ed8ec90db2e4b92
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-06-13 19:18:38 -0300

    Creacion de Server

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const videogamesRoutes = require("./videogamesRoutes");
+const genresRoutes = require("./genresRoutes");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/videogames", () => console.log("hola"));
+router.use("/videogame", () => console.log("hola"));
+router.use("/genres", () => console.log("hola"));
 
 module.exports = router;
