5fe9618c86c6dcc48480626b79401192d3c03374
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-06-15 18:37:42 -0300

    Redux - Configuraciones Iniciales

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,21 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
 import VideogameDetail from "./components/VideogameDetail/VideogameDetail";
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/videgame/:id" element={<VideogameDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/videgame/:id" element={<VideogameDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
